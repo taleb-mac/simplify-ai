9767ff911324d258a7dd8a2a7c203bcdf767f9a3
Cyberistic <Cyb3ristic@gmail.com>
2023-11-19 04:10:27 +0300

    made everything better,

---
+++ app/api/FileToText/route.js
@@ -0,0 +1,120 @@
+"use server";
+import { NextResponse } from "next/server";
+import PDFParser from "pdf2json";
+import fs from "fs-extra";
+
+export async function POST(req) {
+  try {
+    // Parse the FormData from the request
+    const formData = await req.formData();
+    const file = formData.get("file"); // Get the file from FormData
+
+    if (!file) throw new Error("No file provided.");
+
+    switch (file.type) {
+      case "application/pdf":
+        const pdftext = await extractTextFromPDF(file);
+        return new NextResponse(JSON.stringify({ text: pdftext }), {
+          status: 200,
+          headers: { "Content-Type": "application/json" }
+        });
+
+        return new NextResponse(
+          JSON.stringify({ error: "PDF file type not supported yet" }),
+          {
+            status: 400,
+            headers: { "Content-Type": "application/json" }
+          }
+        );
+
+      case "image/png":
+      case "image/jpeg":
+        const text = await extractTextFromImage(file);
+        if (text) {
+          return new NextResponse(JSON.stringify({ text: text }), {
+            status: 200,
+            headers: { "Content-Type": "application/json" }
+          });
+        } else {
+          return new NextResponse(
+            JSON.stringify({ error: "Error reading image" }),
+            {
+              status: 400,
+              headers: { "Content-Type": "application/json" }
+            }
+          );
+        }
+
+      default:
+        console.error("Unsupported file type");
+        return new NextResponse(
+          JSON.stringify({ error: "Unsupported file type" }),
+          {
+            status: 400,
+            headers: { "Content-Type": "application/json" }
+          }
+        );
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return new NextResponse(JSON.stringify({ status: "error", data: error }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+}
+
+const extractTextFromImage = async (imageFile) => {
+  console.log("Extracting text from image...");
+
+  formData.append("file", imageFile); // Replace 'image.jpg' with the actual file name if available
+  formData.append("language", "eng");
+  formData.append("apikey", process.env.OCRSPACE_API);
+
+  try {
+    const response = await fetch("https://api.ocr.space/parse/image", {
+      method: "POST",
+      body: formData
+    });
+
+    const result = await response.json();
+    if (result.OCRExitCode !== 1) {
+      throw new Error(`OCR Error: ${result.ErrorMessage}`);
+    }
+    console.log(result);
+    const text = result.ParsedResults[0].ParsedText;
+    console.log(text);
+    return text;
+  } catch (error) {
+    console.error("Error during OCR.space API call:", error);
+    throw error;
+  }
+};
+
+const extractTextFromPDF = async (pdfFile) => {
+  console.log("Extracting text from PDF...");
+  const buffer = Buffer.from(await pdfFile.arrayBuffer());
+  const tempFilePath = "tempPdfFile.pdf";
+
+  // Write the PDF buffer to a temporary file
+  await fs.writeFile(tempFilePath, buffer);
+
+  // Create a new instance of PDFParser
+  const pdfParser = new PDFParser(this, 1);
+  return new Promise((resolve, reject) => {
+    pdfParser.on("pdfParser_dataError", (errData) => {
+      fs.remove(tempFilePath); // Clean up even if there's an error
+      reject(new Error(errData.parserError));
+    });
+
+    pdfParser.on("pdfParser_dataReady", async (pdfData) => {
+      const text = pdfParser.getRawTextContent();
+      console.log(text);
+      await fs.remove(tempFilePath);
+      resolve(text);
+    });
+
+    // Load the PDF file
+    pdfParser.loadPDF(tempFilePath);
+  });
+};
--- app/components/FileUpload.js
+++ app/components/FileUpload.js
@@ -1,44 +1,58 @@
-'use client';
-import ReadFile from '../api/FileToText';
-import { useState, useEffect } from 'react';
+"use client";
+import { useState, useEffect } from "react";
 
 const FileUpload = () => {
   const [fileText, setFileText] = useState(null);
-  const [FileUpload, setFileUpload] = useState(false);
 
+  const handleFileUpload = async (e) => {
+    console.log("File upload started...");
+    const file = e.target.files[0];
+    let formData = new FormData();
+    formData.set("file", file);
 
-  useEffect(() => {
-    const handleFileSelect = async () => {
-      const selectedFile = document.getElementById("dropzone-file").files[0];
-      if (selectedFile) {
-        try {
-          const text = await ReadFile(selectedFile);
-          setFileText(text);
-        } catch (error) {
-          console.error("Error reading file:", error);
-        }
-      };
-    };
-    console.log("FileUpload component mounted");
-    handleFileSelect();
-  }, [FileUpload]);
+    const text = await fetch("/api/FileToText", {
+      method: "POST",
+      body: formData
+    }).then((res) => res.json());
+  };
 
   return (
     <div className="flex items-center justify-center my-12">
       <div className="w-full max-w-lg p-4 border-2 border-dashed rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600">
-        <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center h-64 cursor-pointer">
+        <label
+          htmlFor="dropzone-file"
+          className="flex flex-col items-center justify-center h-64 cursor-pointer"
+        >
           <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+            <svg
+              className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 16"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+              />
             </svg>
             <p className="mb-2 text-sm text-gray-700 dark:text-gray-300">
-              <span className="font-semibold">Click to upload</span> or drag and drop
+              <span className="font-semibold">Click to upload</span> or drag and
+              drop
             </p>
             <p className="text-xs text-gray-600 dark:text-gray-300">
               PDF, PPTX, or Image files
             </p>
           </div>
-          <input id="dropzone-file" type="file" className="hidden" onChange={() => setFileUpload((current) => !current)}/>
+          <input
+            id="dropzone-file"
+            type="file"
+            className="hidden"
+            onChange={(e) => handleFileUpload(e)}
+          />
         </label>
       </div>
       <div>
